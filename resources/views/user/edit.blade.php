6b96ffb3ae0ff669233c8b36593e5ca2de3bc5a2
unknown <jugo.adis1@gmail.com>
2019-11-19 17:09:11 +0100

    fixed month selector

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -18,180 +18,131 @@
 </section>
 <!-- banner-section end -->
 
+
+<section class="user-edit-section section-padding">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Profile') }}</div>
-                    @if(session()->has('success'))
-                        <div class="alert alert-success alert-dismissable m-3">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-                    @if(session()->has('error'))
-                        <div class="alert alert-danger m-3">
-                            {{ session('error') }}
-                        </div>
-                    @endif
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('user.update', ['id' => $user->id]) }}">
-                            @csrf
-
-                            <div class="form-group row">
-                                <label for="firstName" class="col-md-4 col-form-label text-md-right">{{ __('First Name *') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="firstName" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name', $user->first_name) }}" required autocomplete="first_name" autofocus>
-
-                                    @error('first_name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="lastName" class="col-md-4 col-form-label text-md-right">{{ __('Last Name *') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="lastName" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name', $user->last_name) }}" required autocomplete="last_name" autofocus>
-
-                                    @error('last_name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username *') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="username" type="text" class="form-control" name="username" value="{{ old('username', $user->username) }}" disabled>
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address *') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email', $user->email) }}" disabled>
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('New Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="mobilePhone" class="col-md-4 col-form-label text-md-right">{{ __('Mobile Phone *') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="mobilePhone" type="text" class="form-control @error('mobile_phone') is-invalid @enderror" name="mobile_phone" value="{{ old('mobile_phone', $user->mobile_phone) }}" required autocomplete="mobile_phone" autofocus>
-
-                                    @error('mobile_phone')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="telephone" class="col-md-4 col-form-label text-md-right">{{ __('Telephone') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="telephone" type="text" class="form-control @error('telephone') is-invalid @enderror" name="telephone" value="{{ old('telephone', $user->telephone) }}" autocomplete="telephone" autofocus>
-
-                                    @error('telephone')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="street" class="col-md-4 col-form-label text-md-right">{{ __('Street *') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="street" type="text" class="form-control @error('street') is-invalid @enderror" name="street" value="{{ old('street', $user->street) }}" required autocomplete="street" autofocus>
-
-                                    @error('street')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="postCode" class="col-md-4 col-form-label text-md-right">{{ __('Post Code *') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="postCode" type="text" class="form-control @error('post_code') is-invalid @enderror" name="post_code" value="{{ old('post_code', $user->post_code) }}" required autocomplete="post_code" autofocus>
-
-                                    @error('post_code')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="place" class="col-md-4 col-form-label text-md-right">{{ __('Place *') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="place" type="text" class="form-control @error('place') is-invalid @enderror" name="place" value="{{ old('place', $user->place) }}" required autocomplete="place" autofocus>
-
-                                    @error('place')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Update') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+            <div class="col-lg-10">
+            @if(session()->has('success'))
+                <div class="alert alert-success alert-dismissable m-3">
+                    {{ session('success') }}
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">{{ __('Sidebar') }}</div>
-                    <div class="card-body">
-                       @include('includes.sidebar')
-                    </div>
+            @endif
+            @if(session()->has('error'))
+                <div class="alert alert-danger m-3">
+                    {{ session('error') }}
                 </div>
+            @endif
             </div>
+
+            <form class="cmn-form registration-form" method="POST" action="{{ route('user.update', ['id' => $user->id]) }}">
+                @csrf
+                <div class="frm-group">
+                    <input type="text" id="firstName" placeholder="Vorname *" name="first_name"
+                        value="{{ old('first_name', $user->first_name) }}" required autocomplete="first_name" autofocus>
+                    @error('first_name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="frm-group">
+                    <input type="text" id="lastName" name="last_name" value="{{ old('last_name', $user->last_name) }}" required autocomplete="last_name"
+                        autofocus ">
+                    @error('last_name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="frm-group">
+                    <input type="text" id="username" name="username"
+                        value="{{ old('username', $user->username) }}" disabled>
+                </div>
+
+                <div class="frm-group">
+                    <input type="text" id="email" name="email"
+                        value="{{ old('email', $user->email) }}" disabled>
+                </div>
+
+                <div class="frm-group">
+                    <input type="password" name="password" id="password" required autocomplete="password"
+                        placeholder="Passwort">
+
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="frm-group">
+                    <input type="password" id="password-confirm" placeholder="Passwort bestätigen"
+                        name="password_confirmation">
+                </div>
+
+                <div class="frm-group">
+                    <input id="mobilePhone" type="text" class="form-control @error('mobile_phone') is-invalid @enderror" name="mobile_phone" value="{{ old('mobile_phone', $user->mobile_phone) }}" required autocomplete="mobile_phone" autofocus>
+
+
+                @error('mobile_phone')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+                </div>
+
+                <div class="frm-group">
+                    <input id="telephone" type="text" name="telephone" value="{{ old('telephone', $user->telephone) }}">
+                    @error('telephone')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="frm-group">
+                    <input id="street" type="text" class="@error('street') is-invalid @enderror"
+                        name="street" value="{{ old('street', $user->street) }}" required autocomplete="street" autofocus
+                        placeholder="Strasse" />
+                    @error('street')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="frm-group">
+                    <input id="post-code" type="text" name="post_code"
+                        value="{{ old('post_code', $user->post_code) }}" required autocomplete="post_code" autofocus placeholder="Postleitzahl"/>
+                    @error('post_code')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="frm-group">
+                    <input id="place" type="text" type="text" class="@error('place') is-invalid @enderror"
+                        name="place" value="{{ old('place', $user->place) }}" required autocomplete="place" autofocus placeholder="Ort">
+
+                    @error('place')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="frm-group">
+                    <button type="submit" class="btn btn-danger">{{ __('Update') }}</button>
+                </div>
+
+            </form>
+
+            <div 
         </div>
     </div>
+</section>
+
+
 @endsection
